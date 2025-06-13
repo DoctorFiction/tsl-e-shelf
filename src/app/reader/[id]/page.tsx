fd762468b810ae8dfe9d2b88564991c85ac7852c
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-14 00:12:07 +0300

    Client side reader demo

--- src/app/reader/[id]/page.tsx
+++ src/app/reader/[id]/page.tsx
@@ -3,7 +3,6 @@ type Params = { id: string };
 export default async function ReaderPage({ params }: { params: Params }) {
   const res = await fetch(`https://gutendex.com/books/${params.id}`);
   const book = await res.json();
-
   const htmlUrl = book.formats["text/html"];
 
   return (
@@ -11,11 +10,9 @@ export default async function ReaderPage({ params }: { params: Params }) {
       <div className="p-4 bg-gray-100 border-b">
         <h1 className="text-xl font-bold">{book.title}</h1>
         <p className="text-sm text-gray-600">
-          {book.authors.map((a: any) => a.name).join(", ")}
+          {book.authors.map((a: { name: string }) => a.name).join(", ")}
         </p>
       </div>
-
-      {/* TODO: Try out client side reader */}
       <iframe
         src={htmlUrl}
         className="flex-1 w-full border-0"
