f75afb5bf80c39c7e7766286dc045a5256315348
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-15 20:29:36 +0300

    Header buttons styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-  // TODO add nav progress
   return (
     <div className="border-black/10 border-b  flex items-center justify-between p-4">
       <Link href="/">
@@ -14,11 +13,16 @@ export default function Header() {
         />
       </Link>
       <ul className="flex items-center gap-3">
+        {/* TODO update the nav buttons */}
         <li>
-          <Link href="/">Ana Sayfa</Link>
+          <div className="px-4 py-2 rounded-sm border border-blue-700">
+            <Link href="/">Ana Sayfa</Link>
+          </div>
         </li>
         <li>
-          <Link href="/books">Kitaplar</Link>
+          <div className="px-4 py-2 rounded-sm bg-blue-700 text-white font-bold">
+            <Link href="/books">Kitaplar</Link>
+          </div>
         </li>
         <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-blue-300 rounded-full ">
           <span>HÖ</span>
