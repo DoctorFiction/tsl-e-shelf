cee6d9e1c272e30e615cf765296b6f8b77651ea3
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-15 19:40:23 +0300

    Progress bar

---
+++ src/app/providers/progress-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ProgressProvider } from "@bprogress/next/app";
+
+const ProgressBarProdiver = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ProgressProvider
+      height="4px"
+      color="#3B82F6"
+      options={{ showSpinner: false }}
+      shallowRouting
+    >
+      {children}
+    </ProgressProvider>
+  );
+};
+
+export default ProgressBarProdiver;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
+  // TODO add nav progress
   return (
-    <div className="border-black/10 border-b h-[60px] flex items-center justify-between px-5">
-      {/* TODO: Update logo, add placeholder profile avatar */}
+    <div className="border-black/10 border-b  flex items-center justify-between p-4">
       <Link href="/">
         <Image
           src="https://www.atlaskitap.com/bakim/nobelyayin.png"
@@ -21,7 +21,7 @@ export default function Header() {
           <Link href="/books">Kitaplar</Link>
         </li>
         <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-blue-300 rounded-full ">
-          <span>SY</span>
+          <span>HÖ</span>
         </div>
       </ul>
     </div>
