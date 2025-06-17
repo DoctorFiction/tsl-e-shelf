41f3a2b47983f2601675bf8aa94c8a6b4dc9807e
Hasan Özçelik <doctorfiction@hotmail.com>
2025-06-17 21:17:28 +0300

    Table of contents (toc)

--- src/hooks/use-epub-reader.ts
+++ src/hooks/use-epub-reader.ts
@@ -1,34 +1,43 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import ePub, { Location, Rendition } from "epubjs";
+import ePub, { Book, Location, Rendition, NavItem } from "epubjs";
 
-interface UseEpubReaderReturn {
-  viewerRef: React.RefObject<HTMLDivElement | null>;
+interface IUseEpubReaderReturn {
   location: string | null;
   goNext: () => void;
   goPrev: () => void;
+  goToHref: (href: string) => void;
+  toc: NavItem[];
+  viewerRef: React.RefObject<HTMLDivElement | null>;
 }
 
-export function useEpubReader(url: string): UseEpubReaderReturn {
+export function useEpubReader(url: string): IUseEpubReaderReturn {
   const viewerRef = useRef<HTMLDivElement>(null);
   const renditionRef = useRef<Rendition | null>(null);
   const [location, setLocation] = useState<string | null>(null);
+  const [toc, setToc] = useState<NavItem[]>([]);
 
   useEffect(() => {
     if (!viewerRef.current) return;
 
-    const book = ePub(url);
+    // BOOK
+    const book: Book = ePub(url);
+    book.ready.then(() => {
+      setToc(book.navigation?.toc || []);
+    });
+
+    // RENDITION
     const rendition = book.renderTo(viewerRef.current, {
       width: "100%",
       height: "100%",
       allowScriptedContent: true,
     });
-
     renditionRef.current = rendition;
     rendition.display();
     rendition.on("relocated", (location: Location) => {
       setLocation(location.start.cfi);
     });
 
+    // CLEANUP
     return () => {
       rendition.destroy?.();
       book.destroy?.();
@@ -43,5 +52,16 @@ export function useEpubReader(url: string): UseEpubReaderReturn {
     renditionRef.current?.prev();
   }, []);
 
-  return { viewerRef, location, goNext, goPrev };
+  const goToHref = useCallback((href: string) => {
+    renditionRef.current?.display(href);
+  }, []);
+
+  return {
+    location,
+    goNext,
+    goPrev,
+    goToHref,
+    toc,
+    viewerRef,
+  };
 }
