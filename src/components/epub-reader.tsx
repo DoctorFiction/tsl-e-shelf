af80fe07ab53d19649e8dad96ce0ef7b1b68d36f
Mustafa GULER <mustafa.guler@yandex.com.tr>
2025-06-22 20:35:04 +0300

    epub reader içindeki tuşlar shadcn'e aktarıldı.

--- src/components/epub-reader.tsx
+++ src/components/epub-reader.tsx
@@ -1,55 +1,48 @@
 "use client";
 import { useEpubReader } from "@/hooks/use-epub-reader";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Typography } from "@/components/ui/typography";
+import { Card, CardContent } from "@/components/ui/card";
 
 interface EpubReaderProps {
   url: string;
 }
 
 export default function EpubReader({ url }: EpubReaderProps) {
-  const {
-    viewerRef,
-    goNext,
-    goPrev,
-    goToCfi,
-    searchQuery,
-    setSearchQuery,
-    searchResults,
-  } = useEpubReader(url);
+  const { viewerRef, goNext, goPrev, goToCfi, searchQuery, setSearchQuery, searchResults } = useEpubReader(url);
 
   return (
-    <div>
-      <div className="flex gap-4 p-2 justify-center bg-gray-100 dark:bg-gray-800">
-        <button onClick={goPrev} className="px-4 py-2 bg-blue-500  rounded">
-          ◀ Prev
-        </button>
-        <button onClick={goNext} className="px-4 py-2 bg-blue-500  rounded">
-          Next ▶
-        </button>
-      </div>
+    <Card className="mt-2">
+      <CardContent>
+        <div className="flex gap-4 p-2 justify-center bg-gray-100 dark:bg-gray-800">
+          <Button onClick={goPrev}>◀ Prev</Button>
+          <Button onClick={goNext}>Next ▶</Button>
+        </div>
 
-      <div className="p-2 text-center text-sm text-gray-500">
-        <input
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Search in book..."
-        />
-      </div>
-      <ul>
-        {searchResults.length > 0 && (
-          <p className="font-bold">{searchResults.length} results found:</p>
-        )}
-        {searchResults.map((result, i) => (
-          <li key={i} onClick={() => goToCfi(result.cfi)}>
-            <div className="flex gap-0.5">
-              <p>{i + 1} - </p>
-              <p dangerouslySetInnerHTML={{ __html: result.excerpt }} />{" "}
-              <p className="font-bold">({result.chapterTitle})</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <div ref={viewerRef} className="w-full h-screen" />
-    </div>
+        <div className="p-2 text-center text-sm text-gray-500">
+          <Input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search in book..." />
+        </div>
+        <ul>
+          {searchResults.length > 0 && (
+            <Typography variant="body1" className="font-bold">
+              {searchResults.length} results found:
+            </Typography>
+          )}
+          {searchResults.map((result, i) => (
+            <li key={i} onClick={() => goToCfi(result.cfi)}>
+              <div className="flex gap-0.5">
+                <Typography variant="body2">{i + 1} - </Typography>
+                <Typography variant="body2" dangerouslySetInnerHTML={{ __html: result.excerpt }} />{" "}
+                <Typography variant="body2" className="font-bold">
+                  ({result.chapterTitle})
+                </Typography>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div ref={viewerRef} className="w-full h-screen" />
+      </CardContent>
+    </Card>
   );
 }
