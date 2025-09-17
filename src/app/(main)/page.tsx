e40c62cee99c74a7f8de8bf01c5f4f915b33357f
Mustafa GULER <mustafa.guler@yandex.com.tr>
2025-09-17 17:33:28 +0300

    refactor(page): clean up imports and streamline redirect logic

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,35 @@
-import BookSearchAutocomplete from "@/components/book-search-autocomplete";
-import RecentlyVisitedBooks from "@/components/recently-visited-books";
+// import BookSearchAutocomplete from "@/components/book-search-autocomplete";
+// import RecentlyVisitedBooks from "@/components/recently-visited-books";
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    setTimeout(() => {
+      router.push("/books/local/");
+    }, 1);
+  };
+
+  useEffect(() => {
+    handleRedirect();
+  }, []);
+
   return (
     <div className="text-center pt-12">
       <h1 className="text-3xl capitalize font-bold mb-4">E-shelf</h1>
       <p className="text-[16px] mb-8">Bu ana sayfa... Son kitapları, aramayı vb. ekleyin...</p>
       <div className="max-w-7xl mx-auto mb-10">
         {/* Book Search Autocomplete Component */}
-        <BookSearchAutocomplete />
+        {/* <BookSearchAutocomplete /> */}
       </div>
       <div className="max-w-7xl mx-auto">
         {/* Recently Visited Books Component */}
-        <RecentlyVisitedBooks />
+        {/* <RecentlyVisitedBooks /> */}
       </div>
     </div>
   );
